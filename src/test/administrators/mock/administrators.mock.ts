7fb71e7b91681cf859c0c33490161d1935ad048c
leticia <letidesiderio18@gmail.com>
2022-11-24 15:56:32 -0300

    test(admnistrators): test for read list of admnistrators

--- src/test/administrators/mock/administrators.mock.ts
+++ src/test/administrators/mock/administrators.mock.ts
@@ -1,18 +1,85 @@
-import { IAdministrators, IAdministratorsCreate, IAdministratorsRepository } from "../../../modules/administrators";
+import {
+  IAdministrators,
+  IAdministratorsCreate,
+  IAdministratorsReadListParamsPagination,
+  IAdministratorsRepository,
+} from "../../../modules/administrators";
 import { administratorsMockList } from "./administrators.mockList";
 
-export class AdministratorsServiceMock implements IAdministratorsRepository  {
-    create = jest.fn().mockImplementation(async (params: { administrators: IAdministratorsCreate }): Promise<IAdministrators> => {
+export class AdministratorsServiceMock implements IAdministratorsRepository {
+  create = jest
+    .fn()
+    .mockImplementation(
+      async (params: {
+        administrators: IAdministratorsCreate;
+      }): Promise<IAdministrators> => {
         const { administrators } = params;
-		const { name, email, password, termsOfUse } = administrators;
-		const newAdministrators: IAdministrators = {
-			_id: 'new_id',
-			name,
-			email,
-            password,
-            termsOfUse
-		};
-		return newAdministrators;
-	});
+        const { name, email, password, termsOfUse } = administrators;
+        const newAdministrators: IAdministrators = {
+          _id: "new_id",
+          name,
+          email,
+          password,
+          termsOfUse,
+        };
+        return newAdministrators;
+      }
+    );
+  readList = jest
+    .fn()
+    .mockImplementation(
+      async (params: {
+        query?: string | undefined;
+        page?: number | undefined;
+        regsPerPage?: number | undefined;
+      }): Promise<IAdministrators[]> => {
+        const { query } = params;
 
+        let list = administratorsMockList;
+
+        if (query && query != "") {
+          list = list.filter((administrators) => {
+            let q = query.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+            if (administrators.email?.match(q)) return true;
+            if (administrators.name.match(q)) return true;
+            return false;
+          });
+        }
+
+        return list;
+      }
+    );
+
+  readListCount = jest
+    .fn()
+    .mockImplementation(
+      async (params: IAdministratorsReadListParamsPagination) => {
+        const filtered = this.queryMockList(params);
+
+        return filtered.length;
+      }
+    );
+
+  queryMockList(
+    params: IAdministratorsReadListParamsPagination
+  ): Array<IAdministrators> {
+    const { query } = params;
+    let { page, regsPerPage } = params;
+
+    if (query) {
+      administratorsMockList.filter((administrators: IAdministrators) => {
+        let q = query.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+        if (administrators.name?.match(q)) return true;
+        if (administrators.email.match(q)) return true;
+        return false;
+      });
+    }
+
+    // Pages
+    if (page == null) page = 1;
+    if (regsPerPage == null) regsPerPage = 10;
+    administratorsMockList.slice((page - 1) * regsPerPage, page * regsPerPage);
+
+    return administratorsMockList;
+  }
 }
--- src/test/administrators/mock/administrators.mockList.ts
+++ src/test/administrators/mock/administrators.mockList.ts
@@ -6,4 +6,10 @@ export let administratorsMockList: Array<IAdministrators> = [{
     email: 'email@example.com',
     password: '111111',
     termsOfUse: 'example terms of use'
+}, {
+    _id: 'administrators_new_id',
+    name: 'name_new_admin',
+    email: 'email2@example.com',
+    password: '1235',
+    termsOfUse: 'new example terms of use'
 }]
