42422f57bc8f3e22f3478aa828fbaedd884eb706
leticia <letidesiderio18@gmail.com>
2022-11-25 14:26:45 -0300

    refactor(administratorCreate): identification code for creating administrator

--- src/modules/administrators/useCases/create/administratorCreate.DTO.ts
+++ src/modules/administrators/useCases/create/administratorCreate.DTO.ts
@@ -1,11 +1,11 @@
 import { IAdministrators } from "../../repository/administrators.model";
 
 export type IAdministratorCreateDTO = {
-    name: string;
-	email: string;
-	password: string;
-	confirmPassword: string;
-	termsOfUse: string;
-}
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  termsOfUse: string;
+};
 
 export type IAdministratorCreateResponse = IAdministrators;
--- src/modules/administrators/useCases/create/administratorCreate.useCases.ts
+++ src/modules/administrators/useCases/create/administratorCreate.useCases.ts
@@ -1,83 +1,90 @@
-import { AppError } from 'apperror';
-import { IPasswordProvider } from '../../../../config/providers/crypto';
-import { IAdministrators } from '../../repository/administrators.model';
-import { IAdministratorsRepository } from '../../repository/administrators.repository';
-import { IAdministratorCreateDTO, IAdministratorCreateResponse } from './administratorCreate.DTO';
+import { AppError } from "apperror";
+import { IPasswordProvider } from "../../../../config/providers/crypto";
+import { IAdministrators } from "../../repository/administrators.model";
+import { IAdministratorsRepository } from "../../repository/administrators.repository";
+import {
+  IAdministratorCreateDTO,
+  IAdministratorCreateResponse,
+} from "./administratorCreate.DTO";
 
 /**
  * Creates a new administrator
  */
 export class AdministratorCreateUseCase {
-	/**
-	 * Constructor for AdministratorCreateUseCase
-	 * @param administratorsRepository - Inject a {@link IFormRepository} implementation
-	 */
-	constructor(
-			private administratorsRepository: IAdministratorsRepository,
-		 	private passwordProvider: IPasswordProvider,
-		) {}
+  /**
+   * Constructor for AdministratorCreateUseCase
+   * @param administratorsRepository - Inject a {@link IFormRepository} implementation
+   */
+  constructor(
+    private administratorsRepository: IAdministratorsRepository,
+    private passwordProvider: IPasswordProvider
+  ) {}
 
-	/**
-	 * @param dto - {@link IAdministratorsCreateDTO}
-	 * @returns The created administratorsCreateUseCase
-	 */
-	async exec(dto: IAdministratorCreateDTO): Promise<IAdministratorCreateResponse> {
-		const { name, email, password, confirmPassword, termsOfUse } = dto;
-		if (!name) {
-			throw new AppError({
-				message: 'Parameter "name" was not provided.',
-				ptMessage: 'O parâmetro "name" não foi fornecido.',
-			});
-		}
+  /**
+   * @param dto - {@link IAdministratorsCreateDTO}
+   * @returns The created administratorsCreateUseCase
+   */
+  async exec(
+    dto: IAdministratorCreateDTO
+  ): Promise<IAdministratorCreateResponse> {
+    const { name, email, password, confirmPassword, termsOfUse } = dto;
+    if (!name) {
+      throw new AppError({
+        message: 'Parameter "name" was not provided.',
+        ptMessage: 'O parâmetro "name" não foi fornecido.',
+      });
+    }
 
-        if (!email) {
-			throw new AppError({
-				message: 'Parameter "email" was not provided.',
-				ptMessage: 'O parâmetro "email" não foi fornecido.',
-			});
-		}
-        
-        if (!password) {
-			throw new AppError({
-				message: 'Parameter "password" was not provided.',
-				ptMessage: 'O parâmetro "password" não foi fornecido.',
-			});
-		}
+    if (!email) {
+      throw new AppError({
+        message: 'Parameter "email" was not provided.',
+        ptMessage: 'O parâmetro "email" não foi fornecido.',
+      });
+    }
 
-		if (password != confirmPassword) {
-			throw new AppError({
-				message: 'The passwords are not the same.',
-				ptMessage: 'As senhas não são iguais.',
-			});
-		}
+    if (!password) {
+      throw new AppError({
+        message: 'Parameter "password" was not provided.',
+        ptMessage: 'O parâmetro "password" não foi fornecido.',
+      });
+    }
 
-        if (!termsOfUse) {
-			throw new AppError({
-				message: 'Parameter "termsOfUse" was not provided.',
-				ptMessage: 'O parâmetro "termsOfUse" não foi fornecido.',
-			});
-		}
+    if (password != confirmPassword) {
+      throw new AppError({
+        message: "The passwords are not the same.",
+        ptMessage: "As senhas não são iguais.",
+      });
+    }
 
-		const administratorAlreadyExists = await this.administratorsRepository.readOneByEmail({ email });
-		if (administratorAlreadyExists) {
-			throw new AppError({
-				message: 'An administrator with the specified e-mail already exists.',
-				ptMessage: 'Já existe um administrador com o e-mail especificado.',
-			});
-		}
+    if (!termsOfUse) {
+      throw new AppError({
+        message: 'Parameter "termsOfUse" was not provided.',
+        ptMessage: 'O parâmetro "termsOfUse" não foi fornecido.',
+      });
+    }
 
-		const hash = await this.passwordProvider.generateHash({ password });
+    const administratorAlreadyExists =
+      await this.administratorsRepository.readOneByEmail({ email });
+    if (administratorAlreadyExists) {
+      throw new AppError({
+        message: "An administrator with the specified e-mail already exists.",
+        ptMessage: "Já existe um administrador com o e-mail especificado.",
+      });
+    }
 
-		const createdAdministrators: IAdministrators = await this.administratorsRepository.create({
-			administrators: {
-				name,
-				email,
-				password: hash,
-				confirmPassword,
-                termsOfUse,
-			},
-		});
+    const hash = await this.passwordProvider.generateHash({ password });
 
-		return createdAdministrators;
-	}
+    const createdAdministrators: IAdministrators =
+      await this.administratorsRepository.create({
+        administrators: {
+          name,
+          email,
+          password: hash,
+          confirmPassword,
+          termsOfUse,
+        },
+      });
+
+    return createdAdministrators;
+  }
 }
